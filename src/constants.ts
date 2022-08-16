b8bf8e97992eef2d47424a0b1cb82d0ffc1763de
StarfallV <63696126+StarfallV@users.noreply.github.com>
2022-08-16 19:21:07 +0700

    back to old sdk

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,70 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x6b25A73a8AF9E198986593252E29A317C2312B5D'
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
 
-export const INIT_CODE_HASH = '0x7f26a4760c0537f7bd5a8f3c5c296784681f2ae3f2fe593c297b45c37c82f88f'
+export enum Blockchain {
+  ETHEREUM = 1,
+  BINANCE_SMART_CHAIN = 2,
+  HARMONY = 3
+}
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  BSC_MAINNET = 56,
+  BSC_TESTNET = 97,
+  HARMONY_MAINNET = 1666600000,
+  HARMONY_TESTNET = 1666700000
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x0000000000000000000000000000000000000001',
+  [ChainId.ROPSTEN]: '0x0000000000000000000000000000000000000001',
+  [ChainId.RINKEBY]: '0x0000000000000000000000000000000000000001',
+  [ChainId.GÖRLI]: '0x0000000000000000000000000000000000000001',
+  [ChainId.KOVAN]: '0x0000000000000000000000000000000000000001',
+  [ChainId.BSC_MAINNET]: '0x0000000000000000000000000000000000000001',
+  [ChainId.BSC_TESTNET]: '0xD6D546C6408CD06AC6e86f6e5d03dac41d2f3847',
+  [ChainId.HARMONY_MAINNET]: '0x7D02c116b98d0965ba7B642ace0183ad8b8D2196',
+  [ChainId.HARMONY_TESTNET]: '0x066bafafC84607a956967F6f050bD14A8347952F'
+}
+
+export const INIT_CODE_HASH = '0x162f79e638367cd45a118c778971dfd8d96c625d2798d3b71994b035cfe9b6dc'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0)
 export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
 export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
 export const _997 = JSBI.BigInt(997)
 export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
